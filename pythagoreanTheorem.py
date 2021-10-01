02bdde0b2cc0769b81addf66ab179f448c933ff8
Paul Ukaegbu <pauliukaegbu@gmail.com>
2021-09-30 20:51:45 -0400

    first commit

---
+++ pythagoreanTheorem.py
@@ -0,0 +1,23 @@
+import math
+
+# def reverse(names):
+#     for i in reversed(range(len(names))):
+#         reversedNames = names[i]        
+#         print(reversedNames)
+
+# names = input("Whats your name: ")
+# reverse(names)
+
+def pythagoreanTheorem():
+    get_a = int(input("What's 'a':"))
+    get_b = int(input("What's 'b':"))
+    solve_for_c = (get_a ** 2) + (get_b ** 2)
+    c = f"C = {int(math.sqrt(solve_for_c))}"
+    return c
+
+equation = input("What math equation are you solving today? ")
+if (equation == "pythagorean theorem" or "Pythgorean theorem" or "Pythagorean Theorem"):
+    print(pythagoreanTheorem())
+else:
+    print("Something is wrong. Make sure pythagorean theorem is spelt right and make sure it is all lower case!")
+    
